3961608c596205f74b8974b48f5214e64062d7af
abhijeetpalanki <ajpalanki@gmail.com>
2022-09-25 22:29:03 -0500

    footer link to hero

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { SocialIcon } from "react-social-icons";
 import { motion } from "framer-motion";
+import Link from "next/link";
 
 type Props = {};
 
@@ -46,22 +47,24 @@ function Header({}: Props) {
         />
       </motion.div>
 
-      <motion.div
-        initial={{ x: 500, opacity: 0, scale: 0.5 }}
-        animate={{ x: 0, opacity: 1, scale: 1 }}
-        transition={{ duration: 1.5 }}
-        className="flex flex-row items-center text-gray-300 cursor-pointer"
-      >
-        <SocialIcon
-          fgColor="gray"
-          bgColor="transparent"
-          network="email"
-          className="cursor-pointer"
-        />
-        <p className="uppercase hidden md:inline-flex text-sm text-gray-400">
-          Get In Touch
-        </p>
-      </motion.div>
+      <Link href="#contact">
+        <motion.div
+          initial={{ x: 500, opacity: 0, scale: 0.5 }}
+          animate={{ x: 0, opacity: 1, scale: 1 }}
+          transition={{ duration: 1.5 }}
+          className="flex flex-row items-center text-gray-300 cursor-pointer"
+        >
+          <SocialIcon
+            fgColor="gray"
+            bgColor="transparent"
+            network="email"
+            className="cursor-pointer"
+          />
+          <p className="uppercase hidden md:inline-flex text-sm text-gray-400">
+            Get In Touch
+          </p>
+        </motion.div>
+      </Link>
     </header>
   );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -7,6 +7,8 @@ import Header from "../components/Header";
 import Hero from "../components/Hero";
 import Projects from "../components/Projects";
 import Skills from "../components/Skills";
+import Link from "next/link";
+import profileAbout from "../images/profile-about.jpg";
 
 const Home: NextPage = () => {
   return (
@@ -42,6 +44,18 @@ const Home: NextPage = () => {
       <section id="contact" className="snap-start">
         <ContactMe />
       </section>
+
+      <Link href="#hero">
+        <footer className="sticky bottom-5 w-full cursor-pointer">
+          <div className="flex items-center justify-center">
+            <img
+              className="h-10 w-10 rounded-full filter grayscale hover:grayscale-0"
+              src={profileAbout.src}
+              alt="profile"
+            />
+          </div>
+        </footer>
+      </Link>
     </div>
   );
 };
